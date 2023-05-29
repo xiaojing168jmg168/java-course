e83bad1f1494ac7279e5c38b92c3e9a1bd668d59
Xiaojing Deng <8redfishcreative@gmail.com>
2023-05-29 18:57:52 -0400

    implemet add audio

---
+++ audio/Main.java
@@ -0,0 +1,39 @@
+package audio;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+         Scanner scanner = new Scanner(System.in);
+
+        File file = new File("Manj.wav");
+         AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+         Clip clip = AudioSystem.getClip();
+         clip.open(audioStream);
+
+         String response = "";
+         while(!response.equals("Q")){
+             System.out.println("P = Play, S = Stop, R = Reset, Q = Quit");
+             System.out.println("Enter your choice");
+             response = scanner.next();
+             response = response.toUpperCase();
+
+             switch (response){
+                 case ("P"): clip.start();
+                 break;
+                 case ("S"): clip.stop();
+                 break;
+                 case("R"): clip.setMicrosecondPosition(0);
+                 break;
+                 case("Q"): clip.close();
+                 break;
+                 default:
+                     System.out.println("Not a valid response");
+             }
+         }
+
+        System.out.println("Byeeeee!");
+    }
+}
